b8d5247094f86fff2d42cf2e8d5481ece9edc5d3
Tritania <erikwilson@magnorum.com>
2014-07-05 15:14:34 +0000

    Removed body parser, began sockets implementation.

--- server.js
+++ server.js
@@ -5,15 +5,21 @@
 var express = require("express"),
     app = express(),
     methodOverride = require('method-override'),
-    bodyParser = require('body-parser'),
     errorHandler = require('errorhandler'),
     port = 9090;
 var io = require('socket.io').listen(app.listen(port));
 
 app.use(methodOverride());
-app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
 app.use(errorHandler({
     dumpExceptions: true,
     showStack: true
 }));
+
+console.log("Comedo is initializing...");
+
+io.sockets.on('connection', function (socket) {
+    socket.on('join', function (data) {
+        
+    });
+});
