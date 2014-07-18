0ac3e035ceed1085a0917d89e5581edbd4f00a35
Erik Wilson <erikwilson@magnorum.com>
2014-07-17 23:31:47 -0300

    Added 2d array helper function, more world generation addition.

--- utils/world.js
+++ utils/world.js
@@ -16,9 +16,24 @@ function sumArray(arr) {
     return sum;
 }
 
+function array2d(rows) {
+    "use strict";
+    var arr = [],
+        i;
+    for (i = 0; i < rows; i++) {
+        arr[i] = [];
+    }
+    return arr;
+}
+
 exports.createCentral = function () { //once sent the player can keep the core chunk loaded and pass it any other players that join
     "use strict";
     var core;
     
     return core;
+};
+
+exports.getChunk = function (id, core) { //for use once the core chunk has been generated and the game has started
+    "use strict";
+    
 };
