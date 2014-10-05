7757244f88f3ee7eac49bae696156da2ff82deac
Tritania <erikwilson@magnorum.com>
2014-10-05 19:50:21 +0000

    Minor changes to the login frontend.

--- package.sh
+++ package.sh
@@ -1,6 +1,13 @@
+echo "Downloading / Building node modules"
+npm install
+echo "Cleaning Directory"
 rm comedo comedo.nw
+echo "Copying binaries"
 cp /usr/lib/node-webkit/icudtl.dat ./
 cp /usr/lib/node-webkit/nw.pak ./
-zip -r comedo.nw *
+echo "Packaging"
+zip -rq comedo.nw *
 touch comedo
 cat /usr/bin/nw comedo.nw > comedo && chmod +x comedo 
+echo "Done. Running"
+./comedo
